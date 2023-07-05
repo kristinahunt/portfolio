14ddcc9a20c46928df7aa3354d0d81bdd2270022
kristinahunt <kristinahunt@shaw.ca>
2023-07-05 11:18:03 -0700

    finished featured

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -21,20 +21,21 @@ function Featured({featuredImage}) {
     return (
         <section id='featured'>
             <h2>Featured Projects</h2>
-            {restData.map(project =>
-            <div key={project.id} className="featured-project">
-                
-                {project.featured_media !== 0 && project._embedded['wp:featuredmedia'][0] && 
-                <div className="featured-image" dangerouslySetInnerHTML={featuredImage(project._embedded['wp:featuredmedia'][0])}></div>}
+            <div className="featured-projects-container">
+                {restData.map(project =>
+                <div key={project.id} className="featured-project">
+                    
+                    {project.featured_media !== 0 && project._embedded['wp:featuredmedia'][0] && 
+                    <div className="featured-image" dangerouslySetInnerHTML={featuredImage(project._embedded['wp:featuredmedia'][0])}></div>}
 
-                <Link to={`/projects/${project.slug}`}><h3>{project.title.rendered}</h3></Link>
+                    <Link to={`/projects/${project.slug}`}><h3>{project.title.rendered}</h3></Link>
+                </div>
+                    )}
             </div>
-                )}
             <div className="button-arrow">
                 <Link to='/projects'>View Projects</Link>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
             </div>
-            
         </section>
     );
 }
