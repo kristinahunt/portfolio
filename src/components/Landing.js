37279da714d0713ef241689304cf3284e35d7514
kristinahunt <kristinahunt@shaw.ca>
2023-07-06 11:12:20 -0700

    more styling changes

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -26,9 +26,9 @@ function Landing({restBase}) {
             <h1>{restData.title?.rendered}</h1>
             <div className="content" dangerouslySetInnerHTML={{__html:restData.excerpt?.rendered}}></div>
             <div className="button-arrow">
-                <Link to='/projects'>See My Work</Link>
-                {/* <img src={arrowRight} alt="Arrow pointing right" /> */}
+                <Link to='/projects'>See My Work
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
+                </Link>
             </div>
            
         </section>
