b49a043e59d3d4c9c02b927e8816d5264e17cdab
kristinahunt <kristinahunt@shaw.ca>
2023-07-09 14:04:31 -0700

    fixed button sizes

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -23,7 +23,7 @@ const Accordion = ({coverImage, featuredMedia, title, slug, summary, designTools
                            <p className='summary'>{summary}</p>
                            {designTools !== '' ? <p>Design Tools: {designTools}</p> : undefined}
                            {devTools !== '' ? <p>Development Tools: {devTools}</p> : undefined}
-                           <div className="button-arrow">
+                           <div className="button-arrow-small">
                                 <Link to={`/projects/${slug}`}>View Project
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
                                 </Link>
--- src/pages/SingleProject.js
+++ src/pages/SingleProject.js
@@ -1,7 +1,7 @@
 import { useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
-function Project() {
+function SingleProject() {
 
     const {slug} = useParams();
 
@@ -75,11 +75,18 @@ function Project() {
                         <p className={toggleState === 2 ? "content active-content" : "content"}>
                             {project.acf.development_process}
                         </p>
-                    </div> 
+                    </div>
+
+                    <span className="button-arrow">
+                        <Link to='/projects'>More Projects
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"/></svg>
+                        </Link>
+                    </span>
+      
                 </div>
                     )}
         </section>
     )
 }
 
-export default Project;
+export default SingleProject;
